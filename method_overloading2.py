451b169503f0e429b7f85fef06f8672df2821a64
Hames-1616 <bilquis748@gmail.com>
2024-02-29 12:03:15 +0530

    📦 NEW: overloading

--- method_overloading2.py
+++ method_overloading2.py
@@ -1,3 +1,22 @@
-def sum(dt,*args):
-    if dt == 'int':
+def add(datatype, *args):
+ 
+
+    if datatype == 'int':
         answer = 0
+ 
+
+    if datatype == 'str':
+        answer = ''
+ 
+
+    for x in args:
+ 
+
+        answer = answer + x
+ 
+    print(answer)
+ 
+ 
+
+add('int', 5, 6)
+add('str', 'Hi ', 'Geeks')
